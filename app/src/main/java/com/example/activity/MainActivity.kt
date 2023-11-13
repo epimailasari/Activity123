63236a22b79f3708fb4a0ad97a568a5e77f363b6
NasywaHanifa128@gmail.com <fatmafatimatuzzahra66@gmail.com>
2023-11-13 11:58:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+private val Any.java: Class<*>?
+    get() {}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val Intent = Intent(this, SecondActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+}
